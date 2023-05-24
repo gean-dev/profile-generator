186f1f7e6c7c06d74667d450b412b4b5b361764d
Anon-136 <anonjin44@gmail.com>
2023-05-24 16:05:45 +0700

    :green_heart: fix ci build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://accclub-chula.github.io/profile-card",
-  base: "/profile-card/",
+  site: "https://accclub-chula.github.io",
+  base: "/profile-card",
 });
