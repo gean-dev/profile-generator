e3f4537efb67e695d6de96a424289686dc0d1bd6
Anon-136 <anonjin44@gmail.com>
2023-05-26 03:06:35 +0700

    :lipstick: add background

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import image from "@astrojs/image";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://accclub-chula.github.io",
   base: "/profile-card",
-  integrations: [tailwind(), solidJs()]
+  integrations: [
+    tailwind(),
+    solidJs(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      screens: {
+        xs: "448px",
+      },
       fontFamily: {
         sans: [
           "Inconsolata Variable",
