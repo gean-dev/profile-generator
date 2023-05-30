dd8e8e86f63ec8715cad735de333ee36fcf82305
Anon-136 <anonjin44@gmail.com>
2023-05-30 11:11:43 +0700

    :bug: fix bug in safari

--- src/components/SaveImage.tsx
+++ src/components/SaveImage.tsx
@@ -11,17 +11,22 @@ interface SaveImageProps {
 export const SaveImage: Component<SaveImageProps> = (props) => {
   const onClick = () => {
     const node = document.getElementById("card");
-    const save = async () =>
-      htmlToImage
-        .toPng(node!, {
+    const save = async () => {
+      let dataUrl = "";
+      const minDataLength = 2000000;
+      let i = 0;
+      const maxAttempts = 10;
+      while (dataUrl.length < minDataLength && i < maxAttempts) {
+        dataUrl = await htmlToImage.toPng(node!, {
           filter: (node: HTMLElement) => !node.classList?.contains("no-print"),
-        })
-        .then((dataUrl) => {
-          const link = document.createElement("a");
-          link.download = "profile-card.png";
-          link.href = dataUrl;
-          link.click();
         });
+        i += 1;
+      }
+      const link = document.createElement("a");
+      link.download = "profile-card.png";
+      link.href = dataUrl;
+      link.click();
+    };
     toast.promise(save(), {
       loading: "Loading",
       success: "Success!",
